8cd9ea6dc8ac6cb0ce185bff938df19fa00f510e
zoltan23 <timothy_tassin@hotmail.com>
2020-07-16 16:28:04 -0600

    added styling to about page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import './Card.css';
 
-export default function Card({ title, description, link, github, url }) {
-    console.log('github', github)
+export default function Card({ title, description, github, url }) {
     return (
         <div className="card">
             <div className="card-header">
@@ -10,12 +11,11 @@ export default function Card({ title, description, link, github, url }) {
             </div>
             <div className="card-body">
                 <p>{`${description}`}</p>
-               
             </div>
             <div className="links">
-                    <p>Url: <a href="#">{`${url}`}</a></p>
-                    <p>Github: <a href={github}>{github}</a></p>
-                </div>
+                {url ? <p>Url: <a href="#">{`${url}`}</a></p> : ''}
+                <p>Github: <a href={github}><FontAwesomeIcon icon={faGithub} /></a></p>
+            </div>
         </div>
     )
 }

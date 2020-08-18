a68e25324c741e992d788ad1392170caae2c5bf1
zoltan23 <timothy_tassin@hotmail.com>
2020-08-17 20:47:15 -0600

    added modal

--- Client/src/screens/Test.js
+++ Client/src/screens/Test.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import { Element } from "react-scroll";
+import React, { useState } from 'react'
 import './Test.css'
+import Modal from '../components/Modal'
 
 export default function Test() {
+
+    const modalRef = React.useRef();    
+
+    const openModal = () => {
+        modalRef.current.openModal()
+    }
+
     return (
-<Element name="test" >Test</Element>    
-)
+        <div className="">
+            <button onClick={openModal}>Open Modal</button>
+            <Modal ref={modalRef}>
+               <h1>Modal Header</h1>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quia placeat commodi libero tenetur ipsam itaque recusandae omnis rerum praesentium ducimus sed laborum, minima, exercitationem ullam facilis alias ut. Voluptates?</p>
+            </Modal>
+        </div>
+    )
 }
